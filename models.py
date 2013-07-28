4261b0f251fe8ef1b7996c36f58770881855af1d
omniaura <omniaura5@gmail.com>
2013-07-28 16:10:50 -0700

    added User model

--- models.py
+++ models.py
@@ -1,6 +1,11 @@
 import webapp2, utils
 from google.appengine.ext import db
 
+class User(db.Model):
+    username = db.StringProperty(required=True)
+    password = db.StringProperty(required=True)
+    email = db.StringProperty(required=False)
+
 class Page(db.Model):
     body = db.TextProperty()
     created = db.DateTimeProperty(auto_now_add=True)
